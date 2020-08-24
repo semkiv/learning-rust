b39d8cde5a8e842f487cad9c93418a68dca43a0f
Andrii Semkiv <semkiv@gmail.com>
2020-08-24 12:21:30 +0300

    [The Rust Programming Language] [Chapter 3] Common Programming Concepts
    Variables and mutability.

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    const INITIAL_VALUE : i32 = 5; // a constant
+
+    let x = INITIAL_VALUE;
+    let x = x + 1; // shadowing
+    let x = (x * 2).to_string(); // shadowing again and changing the type
+
+    println!("The value of x is: {}", x);
+}
