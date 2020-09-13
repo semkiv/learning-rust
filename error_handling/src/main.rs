20d131ef784f1232c481a9026c535f92317bc916
Andrii Semkiv <semkiv@gmail.com>
2020-09-13 20:55:42 +0300

    [The Rust Programming Language] [Chapter 9] Error Handling
    Various options for handling recoverable and non-recoverable errors.

---
+++ error_handling/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::ErrorKind;
+use std::io::Read;
+use std::io::Write;
+
+fn main() {
+    let filename = "hello.txt";
+    let mut f = match OpenOptions::new().read(true).write(true).open(filename) {
+        Ok(file) => file,
+        Err(error) => match error.kind() { // different sorts of errors can be handled differently
+            ErrorKind::NotFound => match File::create(filename) {
+                Ok(new_file) => new_file,
+                Err(e) => panic!("Problem opening the file: {:?}.", e),
+            }
+            other_error => panic!("Problem opening the file: {:?}.", other_error),
+        }
+    };
+
+    f.write_all(b"Test").expect("Problem writing the username to the file."); // `expect` method of `Result<T, E>` ensures that we `panic` with the given error message in case of an error (a similar alternative is `unwrap` method that just `panic`s, without any specific message)
+
+    match read_username_from_file() {
+        Ok(s) => println!("Username is {}.", s),
+        Err(e) => panic!("Problem reading the username: {:?}.", e),
+    }
+
+    panic!("crash and burn");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    // the following line that contains `?` operators is essentially equivalent to the following code:
+    // ```
+    // match File::open("hello.txt") {
+    //     Ok(mut f) => {
+    //         match f.read_to_string(&mut s) {
+    //             Ok(_) => Ok(s),
+    //             Err(e) => Err(e),
+    //         }
+    //     },
+    //     Err(e) => return Err(e),
+    // };
+    // ```
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
