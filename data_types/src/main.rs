b4f9422be7110adeb64e1a285e7fa43dceddf0b7
Andrii Semkiv <semkiv@gmail.com>
2021-03-05 20:00:24 +0200

    [The Rust Programming Language] [Chapter 3] Common Programming Concepts
    Rewrote the example application based on Data Types subsection in a more logical manner.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,32 +1,80 @@
 fn main() {
-    // scalar types
-    let _byte_var: i8 = -64; // signed 8-bit
-    let _unsigned_byte_var = b'A'; // unsigned 8-bit (declared using a byte literal)
-    let _short_var = -1024i16; // signed 16-bit (declared using a suffix type)
-    let _unsigned_short_var: u16 = 0xFFFF; // unsigned 16-bit (declared as a hex number)
-    let int_var = 0o7777_7777; // signed 32-bit (default choice for integers, declared as a oct number)
-    let unsigned_int_var = 0b11111111_11111111_11111111_00000000u32; // unsigned 32-bit (declared as a binary number using a suffix type)
-    let _long_var = -100500i64; // signed 64-bit (declared using a suffix type)
-    let _unsigned_long_var: u64 = 100500; // unsigned 64-bit
-    let _long_long_var: i128 = -100500100500; // signed 128-bit
-    let _unsigned_long_long_var = 100500100500u128; // unsigned 128-bit (declared using a suffix type)
-    let _arch_size_var: isize = -64; // signed arch size (32 or 64 bits)
-    let _unsigned_arch_size_var = 64usize; // signed arch size (32 or 64 bits, declared using a suffix type)
-    let float_var = 0.42f32; // 32-bit single precision float (declared using a suffix type)
-    let _double_var: f64 = 0.42; // 64-bit double precision float (default choice for floats)
-    let _bool_var = false; // 8-bit boolean type
-    let char_type = '😻'; // 32-bit Unicode character
+    scalar_types();
+    compound_types();
+}
 
-    //compound types
-    let tuple_var_1 = (unsigned_int_var, float_var, char_type); // tuple type with underlying types being inferred
-    let (_, _, z) = tuple_var_1; // destructuring the tuple
-    println!("{}", z);
+fn scalar_types() {
+    integers();
+    floating_point();
+    booleans();
+    characters();
+}
 
-    let tuple_var_2: (f64, f64) = (f64::from(float_var), f64::from(int_var)); // tuple type with underlying types explicitly specified; implicit conversions are forbidden
-    println!("{}", tuple_var_2.1); // accessing tuple members without destructuring
+fn compound_types() {
+    tuples();
+    arrays();
+}
 
-    let arr = [1, 2, 3]; // array
-    println!("{}", arr[0]); // accessing array elements
-    let arr: [u8; 5] = [0; 5]; // array with the explicitly specialized type initialized using the form [init-expr; size]; making use of shadowing
-    println!("{}", arr[2]); // accessing array elements
+fn integers() {
+    let dec_representation = 97u8; // integers are `i32` by default; an optional type suffix is allowed; valid suffixes are `i8`, `i16`, `i32`, `i64`, `isize` and their unsigned counterparts (`u*`)
+    let hex_representation = 0x61; // prefix `0x` signifies hexadecimal notation
+    let oct_representation = 0o141; // prefix `0o` signifies octal notation
+    let bin_representation = 0b0000_0000_0110_0001; // prefix `0b` signifies binary notation; optional separator `_` is alowed for numeric literals
+    let byte_representation = b'a'; // prefix `b` signifies byte notation, applicable to `u8` only
+
+    assert_eq!(dec_representation, hex_representation);
+    assert_eq!(hex_representation, oct_representation);
+    assert_eq!(oct_representation, bin_representation);
+    assert_eq!(bin_representation, byte_representation);
+
+    overflow();
+}
+
+fn floating_point() {
+    let double = 100_500.000_1; // floating point numbers are `f64` (double precision) by default; separators are allowed
+    let float = 0.42f32; // single precision can be specified through type suffix (or type annotation)
+    assert!(float < double);
+}
+
+fn booleans() {
+    let boolean = true;
+    assert!(boolean);
+}
+
+fn characters() {
+    let character = '😻'; // `char`s literals are specified in single quotes; these represent Unicode Scalar Values (32-bit Unicode "character")
+    assert_eq!(character, '\u{1F63B}'); // `char`s can be specified as Unicode code points
+}
+
+fn tuples() {
+    let transparent_navy: (u8, u8, u8, f64) = (0, 0, 128, 0.5); // should normally be a `struct`, tuple only as an example; alternative form would be `let transparent_navy = (0u8, 0u8, 128u8, 0.5)`; without explicit specifiaction type would be inferred to `(i32, i32, i32, f64)`
+    let (_, _, blue_component, _) = transparent_navy; // destructuring the tuple; since we only care about the blue component, the others are placeholders
+    assert_eq!(blue_component, 128);
+    assert_eq!(transparent_navy.1, 0); // tuple members can be accessed by their index without destructuring
+}
+
+fn arrays() {
+    let arr = [1, 2, 3]; // an array
+    assert_eq!(arr[0], 1); // accessing array elements
+    let arr: [u8; 5] = [42; 5]; // array with the explicitly specialized type initialized using the form [init-expr; size]; alternative form would be simply `let arr = [42u8, 5]`
+    assert_eq!(arr[4], 42);
+}
+
+fn overflow() {
+    let at_limit = 127i8; // Rust uses two's complement and guarantees that `iN` can have values from (-(2 ^ ( N - 1))) to (2 ^ (N - 1) - 1) and `uN` - from 0 to (2 ^ N - 1)
+
+    // at_limit += 1; // this panics in debug mode and two’s complement wraps in release mode; relying on implicit wrapping is considered bad practice - there's a set of explicit alternatives
+
+    let result = at_limit.wrapping_add(1); // `wrapping_*` family of members always wrap
+    assert_eq!(result, -128);
+
+    let result = at_limit.checked_add(1); // `checked_*` family of members returns `Some(T)` if the overflow did not happen or `None` otherwise
+    assert_eq!(result, None);
+
+    let (result, has_overflown) = at_limit.overflowing_add(1); // `overflowing_*` family of members returns the wrapped value and a boolean indicating if the overflow happened
+    assert_eq!(result, -128);
+    assert_eq!(has_overflown, true);
+
+    let result = at_limit.saturating_add(1); // `saturating_*` family of members saturate at the value's minimum or maximum values
+    assert_eq!(result, 127);
 }
