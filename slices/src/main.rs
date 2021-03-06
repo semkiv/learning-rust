27a3f1e7a41ca427ffb7a89876776c1c2f3fafda
Andrii Semkiv <semkiv@gmail.com>
2021-03-07 00:03:45 +0200

    [The Rust Programming Language] [Chapter 4]  The Slice Type
    Made the logic a bit clearer and concise.

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,19 +1,17 @@
 fn main() {
     let s = String::from("Hello, world!");
-    println!("{}", dumb_first_word(&s));
+    assert_eq!(dumb_first_word(&s), 6);
 
-    let _hello = &s[0..5]; // slices
-    let _hello2 = &s[..5]; // 0 can be omitted
-    let _world = &s[6..11];
-    let _world2 = &s[6..]; // ending index can be omitted too
-    let _full = &s[..]; // full slice
+    let hello = &s[..5]; // this is a string slice (`&str`); their range is specified as [stating_index..ending_index]; if starting_index is 0 it can be omitted
+    let world = &s[7..]; // ending index can be omitted too in which case length will be used instead of it
+    println!("{}, {}", hello, world);
 
-    println!("{}", first_word(&s));
-    println!("{}", first_word("Another test string.")); // because string literal is already a slice and we take a slice parameter, this compiles
+    println!("{}", first_word(&s)); // `&String` is implicitly convertible `&str`
+    println!("{}", first_word("Another test string.")); // because a string literal is a slice this works straightaway
 
     let arr = [1, 2, 3, 4, 5];
     let arr_slice = &arr[1..3]; // slices work for arrays too; in this case the type is &[i32]
-    println!("{}", arr_slice[0]);
+    assert_eq!(arr_slice[1], 3);
 }
 
 fn dumb_first_word(s: &String) -> usize {
