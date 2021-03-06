f54d414e2ba4e6d781e0546e0349e3e417b139ce
Andrii Semkiv <semkiv@gmail.com>
2021-03-06 22:20:12 +0200

    [The Rust Programming Language] [Chapter 3] Common Programming Concepts
    Grammar.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -19,7 +19,7 @@ fn integers() {
     let dec_representation = 97u8; // integers are `i32` by default; an optional type suffix is allowed; valid suffixes are `i8`, `i16`, `i32`, `i64`, `isize` and their unsigned counterparts (`u*`)
     let hex_representation = 0x61; // prefix `0x` signifies hexadecimal notation
     let oct_representation = 0o141; // prefix `0o` signifies octal notation
-    let bin_representation = 0b0000_0000_0110_0001; // prefix `0b` signifies binary notation; optional separator `_` is alowed for numeric literals
+    let bin_representation = 0b0000_0000_0110_0001; // prefix `0b` signifies binary notation; optional separator `_` is allowed for numeric literals
     let byte_representation = b'a'; // prefix `b` signifies byte notation, applicable to `u8` only
 
     assert_eq!(dec_representation, hex_representation);
@@ -47,7 +47,7 @@ fn characters() {
 }
 
 fn tuples() {
-    let transparent_navy: (u8, u8, u8, f64) = (0, 0, 128, 0.5); // should normally be a `struct`, tuple only as an example; alternative form would be `let transparent_navy = (0u8, 0u8, 128u8, 0.5)`; without explicit specifiaction type would be inferred to `(i32, i32, i32, f64)`
+    let transparent_navy: (u8, u8, u8, f64) = (0, 0, 128, 0.5); // should normally be a `struct`, tuple only as an example; alternative form would be `let transparent_navy = (0u8, 0u8, 128u8, 0.5)`; without explicit specification type would be inferred to `(i32, i32, i32, f64)`
     let (_, _, blue_component, _) = transparent_navy; // destructuring the tuple; since we only care about the blue component, the others are placeholders
     assert_eq!(blue_component, 128);
     assert_eq!(transparent_navy.1, 0); // tuple members can be accessed by their index without destructuring
