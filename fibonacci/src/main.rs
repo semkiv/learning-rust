624cdddbd2aaa7aab8382cc613c3974bda055179
Andrii Semkiv <semkiv@gmail.com>
2021-03-07 10:15:05 +0200

    [The Rust Programming Language] [Chapter 3] Common Programming Concepts
    Slightly refactored the Fibonacci program.
    Logic is more concise now.
    0 index is now handled correctly.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,24 +1,26 @@
 fn main() {
     let n: u32 = loop {
-        println!("Enter a number:");
+        println!("Enter the index:");
 
-        let mut number = String::new();
+        let mut index = String::new();
 
         std::io::stdin()
-            .read_line(&mut number)
+            .read_line(&mut index)
             .expect("Failed to read line");
 
-        match number.trim().parse() {
-            Ok(num) => break num,
-            Err(_) => println!("'{}' is not a valid number 🤷‍♂️", number.trim())
+        let index = index.trim();
+
+        match index.parse() {
+            Ok(idx) => break idx,
+            Err(_) => println!("'{}' is not a valid index 🤷‍♂️", index),
         };
     };
 
-    let mut result: u128 = 1;
-    let mut prev: u128 = 1;
-    let mut prev_prev: u128;
+    let mut result = 0u128;
+    let mut prev = 1;
+    let mut prev_prev;
 
-    for _ in 3..n + 1 {
+    for _ in 0..n {
         prev_prev = prev;
         prev = result;
         result = prev + prev_prev;
