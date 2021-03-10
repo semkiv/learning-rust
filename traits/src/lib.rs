6ca49fb284fd82f7055d8507863b957c03858c02
Andrii Semkiv <semkiv@gmail.com>
2021-03-10 20:59:11 +0200

    [The Rust Programming Language] [Chapter 10] Generic Types, Traits, and Lifetimes
    Clearer logic.
    Better comments.
    Formatting.

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,10 +1,21 @@
-// A trait definition.
+// a trait definition
+// traits that are meant to be implemented by the external code must be marked as `pub`
 pub trait Summary {
     fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author()) // default implementation (optional)
+        format!("(Read more from {}...)", self.summarize_author()) // default implementation (optional); can be kept or overridden
     }
 
-    fn summarize_author(&self) -> String;
+    fn summarize_author(&self) -> String; // this function will have to be implemented by all classes that implement `Summary`
+}
+
+// a function that takes a traits as a parameter
+// the `impl` keyword together with the trait name can be used for this
+// however it is just a syntactic sugar for an equivalent longer form:
+// `fn notify<T: Summary>(item: &T)`.
+// the longer form might be actually more compact if we have multiple repeating traits
+// multiple traits can be specified with the `+` syntax, e.g. `&(impl Summary + Display)` or, equivalently, `T: Summary + Display
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
 }
 
 pub struct NewsArticle {
@@ -14,15 +25,11 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-// An implementation of the trait.
+// an implementation of the trait
+// one restriction to note with trait implementations is that we can implement a trait on a type only if either the trait or the type is local to the crate
+// we can’t implement external traits on external types, for example, we can’t implement the `Display` trait on `Vec<T>`
 impl Summary for NewsArticle {
+    // `NewsArticle` overrides default implementation of `summarize`
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
@@ -32,34 +39,27 @@ impl Summary for NewsArticle {
     }
 }
 
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
 
+impl Summary for Tweet {
+    // `Tweet` only implements mandatory `summarize_author` leaving `summarize` implementation default
     fn summarize_author(&self) -> String {
         format!("@{}", self.username)
     }
 }
 
-// A function that takes a traits as a parameter.
-// The `impl` keyword together with the trait name can be used for this.
-// However it is just a syntactic sugar for an equivalent longer form:
-// `fn notify<T: Summary>(item: &T)`.
-// The longer form might be actually more compact if we have multiple repeating traits.
-// Multiple traits can be specified with the `+` syntax, e.g. `&(impl Summary + Display)` or, equivalently, `T: Summary + Display`.
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-// Functions can return traits.
-// This is very handy in iterators and closures implementations.
+// functions can return traits
+// this is very handy in iterators and closures implementations
+// however, you can only use `impl Trait` if you’re returning a single type, i.e. either `Tweet` or `NewsArticle` in this case, not sometimes one and sometimes another (for example depending on a switch)
 pub fn returns_summarizable() -> impl Summary {
     Tweet {
         username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
+        content: String::from("of course, as you probably already know, people"),
         reply: false,
         retweet: false,
     }
--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,9 +1,6 @@
-use traits::NewsArticle;
-use traits::Summary;
-use traits::Tweet;
+use traits::{NewsArticle, Summary, Tweet};
 
-use core::fmt::Debug;
-use core::fmt::Display;
+use core::fmt::{Debug, Display};
 
 fn main() {
     let tweet = Tweet {
@@ -19,7 +16,9 @@ fn main() {
         headline: String::from("Penguins win the Stanley Cup Championship!"),
         location: String::from("Pittsburgh, PA, USA"),
         author: String::from("Iceburgh"),
-        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best hockey team in the NHL.",
+        ),
     };
 
     println!("New article available! {}", article.summarize());
@@ -29,8 +28,9 @@ fn main() {
 }
 
 fn _some_function<T, U>(_t: &T, _u: &U) -> i32
-    where T: Display + Clone, // `where` clauses can be used to simplify the signature of complex generic functions; in this particular case the standard signature would look like `fn _some_function<T: Display + Clone, U: Clone + Debug>(_t: &T, _u: &U) -> i32`
-          U: Clone + Debug,
+where
+    T: Display + Clone, // `where` clauses can be used to simplify the signature of complex generic functions; in this particular case the standard signature would look like `fn _some_function<T: Display + Clone, U: Clone + Debug>(_t: &T, _u: &U) -> i32`
+    U: Clone + Debug,
 {
     42
 }
