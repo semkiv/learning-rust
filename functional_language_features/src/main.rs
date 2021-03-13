18cc2879652eee546d18743246cd8b273412bd5d
Andrii Semkiv <semkiv@gmail.com>
2021-03-14 00:01:20 +0200

    [The Rust Programming Language] [Chapter 13] Functional Language Features: Iterators and Closures
    Better examples.
    Better comments.
    Formatting.

--- functional_language_features/src/main.rs
+++ functional_language_features/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let do_nothing_variants: Vec<&dyn Fn(u32) -> u32> = vec![ // `Fn` trait is the most relaxed, an instance of a class implementing this trait can only immutably borrow values from the enviroment; similar traits are `FnMut` (mutably borrows the values) and `FnOnce` (moves the values)
+    // `Fn` trait is the most relaxed, an instance of a class implementing this trait can only immutably borrow values from the environment; similar traits are `FnMut` (mutably borrows the values) and `FnOnce` (moves the values)
+    let do_nothing_variants: Vec<&dyn Fn(u32) -> u32> = vec![
         &(|x: u32| -> u32 { x }), // fully annotated closure
         &(|x| { x }), // parameter and return types are optional and can usually be inferred
         &(|x| x), // if a closure contains just one statement, braces enclosing the body are optional too
@@ -9,15 +10,15 @@ fn main() {
         println!("{}", func(42));
     }
 
-    let moved_into = move |s| s; // if we want to force the closure to take ownership of the values it uses in the environment, we can use the `move` keyword before the parameter list; this technique is mostly useful when passing a closure to a new thread to move the data so it’s owned by the new thread
+    let moved_into = move |s| s; // if we want to force the closure to take ownership of the values it uses in the environment, we can use the `move` keyword before the parameter list; this technique is mostly useful when passing a closure to a new thread to move the data so it’s owned by the new thread; note: `move` closures may still implement `Fn` or `FnMut`, even though they capture variables by `move` - this is because the traits implemented by a closure type are determined by what the closure does with captured values, not how it captures them (the `move` keyword only specifies the latter)
     let showcase = String::from("Witness this!");
     println!("{}", moved_into(showcase));
 
     let sum: u32 = CountToFive::new()
         .zip(CountToFive::new().skip(1)) // iterators in Rust are lazy, so nothing is evaluated at this point...
         .map(|(a, b)| a * b)
-        .filter(|x| x % 2 == 0) // ...or even here
-        .sum(); // `sum` method consumes the iterator and now the evaluation actually takes place and we get the sum of the squared even elements
+        .filter(|x| x % 3 == 0) // ...or even here
+        .sum(); // `sum` method consumes the iterator and now the evaluation actually takes place and we get the sum of the adjacent products divisible by three
     println!("{}", sum);
 }
 
@@ -34,7 +35,8 @@ impl CountToFive {
 impl Iterator for CountToFive {
     type Item = u32; // `Iterator` trait has an associated type `Item` that corresponds to the type of the dereferenced iterator
 
-    fn next(&mut self) -> Option<Self::Item> { // `next` method is the only method of the `Iterator` trait that has to be implemented; most of the others are implemented in terms of this method
+    // `next` method is the only method of the `Iterator` trait that has to be implemented; most of the others are implemented in terms of this method
+    fn next(&mut self) -> Option<Self::Item> {
         if self.count < 5 {
             self.count += 1;
             Some(self.count)
