86be77a5b44c75f99de8e6997ca5ecd1cda48e29
Andrii Semkiv <semkiv@gmail.com>
2021-03-28 20:16:55 +0300

    [The Rust Programming Language] [Chapter 18] Patterns and Matching
    Added some notes about refutable and irrefutable patterns.

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -11,6 +11,8 @@ fn if_let_expressions() {
     let is_tuesday = false;
     let age: Result<u8, _> = "34".parse();
 
+    // patterns in Rust can be refutable (such that the matching can fail) and irrefutable (such that always match)
+    // `if let` accepts both refutable and irrefutable pattern, but the compiler will warn about the latter because they're usually not much of use
     if let Some(color) = favorite_color {
         println!("Using your favorite color, {}, as the background", color);
     // `if let` can be combined with `else if`...
@@ -36,6 +38,7 @@ fn while_let_conditional_loops() {
     stack.push(3);
 
     // `while let` can be used similarly to `if let` - the loop will continue as long as the pattern matches
+    // just as `if let` it accepts both refutable and irrefutable patterns with irrefutable ones being warned about by the compiler
     while let Some(top) = stack.pop() {
         println!("{}", top);
     }
@@ -45,6 +48,7 @@ fn for_loops() {
     let v = vec!['a', 'b', 'c'];
 
     // in a `for` loop, the pattern is what directly follows the keyword `for` up until the keyword `in`
+    // `for` loops only accept irrefutable patterns
     for (index, value) in v.iter().enumerate() {
         println!("{} is at index {}", value, index);
     }
@@ -52,14 +56,16 @@ fn for_loops() {
 
 fn let_statements() {
     // a `let` statement is also a place where pattern matching takes place; formally it looks like `let PATTERN = EXPRESSION;`
-    let (x, _, z, ..) = (1, 2, 3, 4, 5); // we can use `_` (to ignore a single value) or `..`(to ignore the rest)
-    println!("x: {}, z: {}", x, z);
+    // `let` statements only accept irrefutable patterns
+    let (x, y, z) = (1, 2, 3);
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }
 
 fn function_parameters() {
     let point = (3, 5);
 
     // pattern matching happens in function parameters too, be it a regular function or a closure
+    // function parameters can be only irrefutable patterns
     let coordinates_printer = |&(x, y): &(i32, i32)| println!("Current location: ({}, {})", x, y);
 
     coordinates_printer(&point);
